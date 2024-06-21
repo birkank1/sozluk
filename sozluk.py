890a49b456776c1f5c8e76346c20bb17a79089e2
birkank1 <birkan.cagan.2009@gmail.com>
2024-06-21 21:56:05 +0300

    Update sozluk.py

--- sozluk.py
+++ sozluk.py
@@ -1,8 +1,10 @@
 meme_dict = {
             "MVP":"En değerli oyunucu"
+            "GG":"İyi oyundu"
 }
 kelime=input("kelime girin")
 if kelime in meme_dict.keys():
+            for kelime in range (5):
     print(meme_dict[kelime])
 else:
     print("kelime listede yok")
